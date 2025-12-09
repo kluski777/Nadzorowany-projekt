5ef8ca56f2614c6ae0772a25030f7073e543c3f6
Blindeen <kuba.szmajda@gmail.com>
2025-12-09 17:49:21 +0100

    feature: add scaler before clusterizer

--- src/data/generate_clusters.py
+++ src/data/generate_clusters.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pathlib import Path
+import joblib
 
 from models import Clusterizer
 from utils import load_latent_spaces
@@ -7,6 +8,7 @@ from utils import load_latent_spaces
 
 def generate_clusters(input_dir: str, checkpoint_path: str, splits: list[str], output_dir: str):
     clt = Clusterizer.load(checkpoint_path)
+    scaler = joblib.load(f"{Path(checkpoint_path).parent}/scaler.pkl")
 
     output_path = Path(output_dir)
     output_path.mkdir(parents=True, exist_ok=True)
@@ -15,9 +17,11 @@ def generate_clusters(input_dir: str, checkpoint_path: str, splits: list[str], o
         latent_components = load_latent_spaces(input_dir, split)
 
         full_latent_components = latent_components["full"]
+        full_latent_components = scaler.transform(full_latent_components)
         full_clusters = clt.predict(full_latent_components)  # Shape: (n_samples,)
 
         cut_latent_components = latent_components["cut"]
+        cut_latent_components = scaler.transform(cut_latent_components)
         cut_clusters = clt.predict(cut_latent_components)  # Shape: (n_samples,)
 
         indices = latent_components["indices"]
--- src/training/fit_clusterizer.py
+++ src/training/fit_clusterizer.py
@@ -1,3 +1,6 @@
+import joblib
+from sklearn.preprocessing import MinMaxScaler
+
 from models import Clusterizer
 from utils import load_latent_spaces
 
@@ -6,6 +9,18 @@ def fit_clusterizer(input_dir: str, output_dir: str, n_clusters: int) -> None:
     latent_components = load_latent_spaces(input_dir, "train")
     full_latent_components = latent_components["full"]  # Shape: (n_samples, n_components)
 
+    print("Fitting scaler and transforming latent components...")
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    full_latent_components = scaler.fit_transform(full_latent_components)
+    joblib.dump(scaler, f"{output_dir}/scaler.pkl")
+
+    print(f"Scaler has been saved to {output_dir}/scaler.pkl")
+
+    print("Fitting clusterizer...")
+
     cl = Clusterizer(n_clusters=n_clusters)
     cl.fit(full_latent_components)
     cl.save(output_dir=output_dir, filename="clusterizer")
+
+    print(f"Clusterizer with {n_clusters} clusters has been saved to {output_dir}/clusterizer.pkl")
